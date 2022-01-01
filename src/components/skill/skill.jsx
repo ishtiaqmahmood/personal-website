501adf004c773b2eab27343facf565e75321dc69
ishtiaqmahmood <ishtiaqmahmood45@gmail.com>
2022-01-01 17:34:12 +0600

    improved ui

--- src/components/skill/skill.jsx
+++ src/components/skill/skill.jsx
@@ -6,26 +6,75 @@ const Skill = () => {
       <div className="h">
         <h4 className="h1">TOP EXPERTISE</h4>
       </div>
-      
-      <div id="skills">
-        <ul>
-          <li>Google Maps API</li>
-          <li>JS Charts</li>
-          <li>AWS (RDS/S3)</li>
-          <li>Heroku</li>
-          <li>HTML/CSS</li>
-        </ul>
-      </div>
-      <div class="card">
-        <div class="container">
-          <h5 className="h3">Programming Language</h5>
-          <ul>
-            <li>Python</li>
-            <li>Django</li>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>Postgres</li>
-          </ul>
+      <div className="g">
+        <div className="row">
+          <div className="column">
+            <div className="card">
+              <h3>Programming language:</h3>
+              <p>C</p>
+              <p>C++</p>
+              <p>Javascript</p>
+              <p>Php</p>
+              <p>Python</p>
+              <p>Solidity</p>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <h3>Web Language:</h3>
+              <p>HTML</p>
+              <p>CSS</p>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <h3>Databases:</h3>
+              <p>MySQL</p>
+              <p>MongoDB</p>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <h3>Web Framework:</h3>
+              <p>React</p>
+              <p>Node.js</p>
+              <p>Express.js</p>
+              <p>Django</p>
+              <p>Bootstrap</p>
+              <p>Material-ui</p>
+              <p>Tailwind css</p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="card">
+              <h3>Blokchain Framework:</h3>
+              <p>Truffle</p>
+              <p>Hardhat</p>
+              <p>Web3.js</p>
+              <p>Tronweb</p>
+              <p>ethers.js</p>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <h3>Cloud:</h3>
+              <p>AWS</p>
+              <p>Google Cloud</p>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <h3>Other:</h3>
+              <p>Git</p>
+              <p>Linux</p>
+              <p>Redis</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
